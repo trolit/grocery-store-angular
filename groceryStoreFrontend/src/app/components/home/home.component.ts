078d22d192dbd35e0d8aed9e080646e588b84904
Paweł Idzikowski <iddziku@gmail.com>
2020-11-02 18:17:57 +0100

    Add animation for start shopping btn

--- groceryStoreFrontend/src/app/components/home/home.component.ts
+++ groceryStoreFrontend/src/app/components/home/home.component.ts
@@ -1,11 +1,36 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('dismiss', [
+        state('closed', style({
+            opacity: 1,
+        })),
+        state('open', style({
+            opacity: 0,
+        })),
+        transition('closed => open', animate(450))
+    ]),
+]
 })
 
 export class HomeComponent {
   title = 'groceryStoreFrontend';
+  isShoppingBtnClicked = false;
+  state = "closed";
+
+  triggerAnimation() : void {
+    this.state = "open";
+    this.hideStartShoppingBtn();
+  }
+
+  hideStartShoppingBtn() : void {
+    setTimeout(() => {
+      this.isShoppingBtnClicked = true;
+    }, 500);
+  }
 }
