9d5d8a5e48819c23be817b5c8b31d7817b48c4cb
Paweł Idzikowski <iddziku@gmail.com>
2020-11-03 01:15:55 +0100

    Use getProducts func and add basic cards

--- groceryStoreFrontend/src/app/components/products/products.component.ts
+++ groceryStoreFrontend/src/app/components/products/products.component.ts
@@ -5,20 +5,42 @@ import { ProductService } from 'src/app/services/product/product.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
-  constructor(private productService: ProductService) { }
   products: Product[];
+  cols: number;
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.getProducts();
+    this.defineColsNumber(window.innerWidth);
+  }
+
+  onScreenResize(event: UIEvent): void {
+    const currentWidth = (event.target as Window).innerWidth;
+    this.defineColsNumber(currentWidth);
   }
 
   getProducts(): void {
-    this.productService.getProducts().subscribe(res => {
+    this.productService.getProducts().subscribe((res) => {
       this.products = res;
+      console.log(this.products);
     });
+  }
+
+  private defineColsNumber(currentWidth: number): void {
+    if (currentWidth <= 800) {
+      this.cols = 1;
+    } else if (currentWidth > 800) {
+      this.cols = 3;
+    } else if (currentWidth >= 1200) {
+      this.cols = 4;
+    } else if (currentWidth >= 1800) {
+      this.cols = 5;
+    } else {
+      this.cols = 6;
+    }
   }
 }
