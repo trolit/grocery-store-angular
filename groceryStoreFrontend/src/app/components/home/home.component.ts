8ffc910d41f2076be9d324cf71b2811e9190b803
Paweł Idzikowski <iddziku@gmail.com>
2020-11-05 00:59:40 +0100

    Simplify home component ts, use RxJS timer instead of setTimeout

--- groceryStoreFrontend/src/app/components/home/home.component.ts
+++ groceryStoreFrontend/src/app/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,39 +9,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss'],
   animations: [
     trigger('dismiss', [
-        state('closed', style({
-            opacity: 1,
-        })),
-        state('open', style({
-            opacity: 0,
-        })),
-        transition('closed => open', animate(450))
+      state(
+        'closed',
+        style({
+          opacity: 1,
+        }),
+      ),
+      state(
+        'open',
+        style({
+          opacity: 0,
+        }),
+      ),
+      transition('closed => open', animate(450)),
     ]),
-]
+  ],
 })
-
 export class HomeComponent {
   title = 'groceryStoreFrontend';
   isShoppingBtnClicked = false;
-  state = "closed";
+  state = 'closed';
   areProductsVisible: boolean;
 
-  triggerAnimation() : void {
-    this.state = "open";
-    this.hideStartShoppingBtn();
-  }
-
-  hideStartShoppingBtn() : void {
-    setTimeout(() => {
-      this.isShoppingBtnClicked = true;
-    }, 500);
+  triggerAnimation(): void {
+    this.state = 'open';
+    timer(800)
+      .pipe(first())
+      .subscribe(() => {
+        this.isShoppingBtnClicked = true;
+      });
   }
 
   scrollToProducts(): void {
     this.areProductsVisible = true;
-    setTimeout(() => {
-      const element = document.querySelector("#products")
-      if (element) element.scrollIntoView({ behavior: 'smooth', block: 'start' })
-    }, 500);
+    timer(500)
+      .pipe(first())
+      .subscribe(() => {
+        const element = document.querySelector('#products');
+        if (element) element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      });
   }
 }
