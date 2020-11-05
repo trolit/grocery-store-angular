d391ec90adcd66a20af674cb23bd4c8211ee967b
Paweł Idzikowski <iddziku@gmail.com>
2020-11-05 01:14:05 +0100

    Format & adjust spec.ts files

--- groceryStoreFrontend/src/app/components/home/home.component.spec.ts
+++ groceryStoreFrontend/src/app/components/home/home.component.spec.ts
@@ -1,35 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './home.component';
+import { HomeComponent } from './home.component';
 
-describe('AppComponent', () => {
+describe('HomeComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [HomeComponent],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HomeComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'groceryStoreFrontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HomeComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('groceryStoreFrontend');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HomeComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('groceryStoreFrontend app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'groceryStoreFrontend app is running!',
+    );
   });
 });
