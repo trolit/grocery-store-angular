85c13e8f720a465d6872c5dc5bc02a427faf4c91
Paweł Idzikowski <iddziku@gmail.com>
2020-11-25 00:59:43 +0100

    Update snackbar createDismissBtn method

--- groceryStoreFrontend/src/app/handlers/snackbarHandler.ts
+++ groceryStoreFrontend/src/app/handlers/snackbarHandler.ts
@@ -6,6 +6,7 @@ import {
   MatSnackBarRef,
   MatSnackBarVerticalPosition,
 } from '@angular/material/snack-bar';
+import { CommonMethodsHandler } from './commonMethodsHandler';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +15,7 @@ export class SnackBarHandler {
   private horizontalPosition: MatSnackBarHorizontalPosition = 'center';
   private verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  constructor(private snackBar: MatSnackBar) {}
+  constructor(private snackBar: MatSnackBar, private commonMethodsHandler: CommonMethodsHandler) {}
 
   openSnackBarWithMessage(message: string, color = 'custom-snackbar-1'): void {
     const snackBarRef = this.snackBar.open(message, 'Close', {
@@ -44,6 +45,7 @@ export class SnackBarHandler {
   }
 
   private createDismissBtn<T>(snackbarRef: MatSnackBarRef<T>): HTMLButtonElement {
+    const commondMethodsHandlerRef = this.commonMethodsHandler;
     const btn = document.createElement('button');
     btn.setAttribute('class', 'mat-focus-indicator mat-raised-button mat-button-base mat-basic');
     btn.setAttribute('color', 'basic');
@@ -51,6 +53,7 @@ export class SnackBarHandler {
     btn.textContent = 'Close';
     btn.onclick = function () {
       snackbarRef.dismiss();
+      commondMethodsHandlerRef.hideOfflineServerLayer();
     };
     return btn;
   }
